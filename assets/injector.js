7d7d7458f53b259615cb70ab7ee26d79ed4c6639
Oliver Green <green2go@gmail.com>
2015-12-17 10:51:00 +0000

    Added fadeIn & fadeOut to animation list.

--- assets/injector.js
+++ assets/injector.js
@@ -7,6 +7,8 @@
             'bounceInLeft',
             'bounceInUp',
             'bounceInDown',
+            'fadeIn',
+            'fadeOut',
             'fadeInRight',
             'fadeInLeft',
             'fadeInUp',
