0f7eca709ce6dd79a8409be72e70a81bb71f9c29
Oliver Green <green2go@gmail.com>
2015-12-19 13:59:56 +0000

    Bumped version.

--- controller.php
+++ controller.php
@@ -15,7 +15,7 @@ class Controller extends Package
 {
     protected $pkgHandle = 'css_animations';
     protected $appVersionRequired = '5.7.1';
-    protected $pkgVersion = '0.9.0';
+    protected $pkgVersion = '0.9.1';
 
     public function getPackageName() 
     {
